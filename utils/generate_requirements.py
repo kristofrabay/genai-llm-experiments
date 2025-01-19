d75731e1258813d35510490101f20d788ef05d50
kristofrabay <kristof.rabay@hiflylabs.com>
2025-01-19 22:50:48 +0100

    update reqs.txt

--- utils/generate_requirements.py
+++ utils/generate_requirements.py
@@ -101,6 +101,7 @@ def main():
             if package in installed_packages:
                 f.write(f'{package}=={installed_packages[package]}\n')
             else:
+                f.write(f'{package}  # Not installed\n')
                 print(f"Warning: Package '{package}' is imported but not installed")
     
     print(f"\nDone! Found {len(third_party_imports)} third-party packages.")
